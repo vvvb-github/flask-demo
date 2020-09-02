0949faa75c964067ce349b0324ea20deafb903ea
gaoshengyi09018133 <1405205313@qq.com>
2020-09-02 18:34:21 +0800

    flask demo

--- config.py
+++ config.py
@@ -2,22 +2,22 @@
 
 
 # 数据库主机
-mysql_host = '64.64.228.191'
+mysql_host = ''
 
 # 数据库端口，mysql默认均为3306
 mysql_port = 3306
 
 # 数据库用户名
-mysql_user = 'vvvb'
+mysql_user = ''
 
 # 数据库密码
-mysql_pass = 'vvvb100'
+mysql_pass = ''
 
 # 数据库名
-mysql_name = 'kxhome'
+mysql_name = ''
 
 # 服务器主机
-server_host = '64.64.228.191'
+server_host = ''
 
 # 服务器端口
-server_port = 8085
+server_port = 5000
